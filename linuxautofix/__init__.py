85b20d2b0d36a8aaeb3f5370e3185a0b6faed04e
Aleksandr Suvorov <myhackband@gmail.com>
2021-01-19 14:06:59 +0700

    Fix errors

--- linuxautofix/__init__.py
+++ linuxautofix/__init__.py
@@ -8,7 +8,7 @@ __version__ = '1.0.0'
 __author__ = 'Aleksandr Suvorov'
 __description__ = 'CLI utility for automatic command execution, ' \
                   'and auto-tuning Linux distributions after installation'
-__url__ = 'https://githib.com/mysmarthub'
+__url__ = 'https://github.com/mysmarthub'
 __donate__ = 'Donate: https://yoomoney.ru/to/4100115206129186'
 __paypal__ = 'https://paypal.me/myhackband'
 __copyright__ = 'Copyright © 2020-2021 Aleksandr Suvorov'
